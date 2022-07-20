9be5de8444b458fe6ded4df9872f98c359ceeb9f
KoLiBer <koliberr136a1@gmail.com>
2022-07-20 12:11:25 +0430

    fix: upgrade terraform lock file

--- main.tf
+++ main.tf
@@ -5,9 +5,17 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 3.4.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3.2"
+    }
     hcloud = {
       source  = "hetznercloud/hcloud"
       version = "~> 1.35.0"
+    }
+    k8sbootstrap = {
+      source  = "nimbolus/k8sbootstrap"
+      version = "0.1.2"
     }
   }
 }
