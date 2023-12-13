206d8270a93711d217d560b12a9b3994a4999ef2
KoLiBer <koliberr136a1@gmail.com>
2023-12-14 01:25:31 +0330

    fix: add firewall ssh rule

--- firewall.tf
+++ firewall.tf
@@ -2,6 +2,13 @@ resource "hcloud_firewall" "this" {
   name = var.name
 
   rule {
+    port        = "22"
+    protocol    = "tcp"
+    direction   = "in"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+    description = "SSH Inbound Traffic"
+  }
+  rule {
     protocol        = "icmp"
     direction       = "out"
     destination_ips = ["0.0.0.0/0", "::/0"]
