bf9c278c14ddf5834fa36bdcb24d629ea5f6ebc6
KoLiBer <koliberr136a1@gmail.com>
2023-12-15 18:57:51 +0330

    fix: add cluster-cidr and service-cidr configs

--- cluster.tf
+++ cluster.tf
@@ -1,6 +1,8 @@
 locals {
   server_configs = {
     "disable-cloud-controller" = "true"
+    "cluster-cidr"             = "10.42.0.0/16"
+    "service-cidr"             = "10.43.0.0/16"
     "disable"                  = ["local-storage", "servicelb", "traefik"]
     "tls-san"                  = [hcloud_load_balancer_network.this.ip, hcloud_load_balancer.this.ipv4]
 
@@ -17,57 +19,9 @@ locals {
   }
 }
 
-data "hcloud_image" "this" {
-  name        = "ubuntu-22.04"
-  most_recent = true
-}
-
-resource "hcloud_placement_group" "this" {
-  name = var.name
-  type = "spread"
-}
-
-resource "hcloud_server" "this" {
-  lifecycle {
-    ignore_changes = [image]
-  }
-
-  for_each = merge(
-    { for key, val in var.servers : "server_${key}" => merge(val, { exec = "server" }) },
-    { for key, val in var.agents : "agent_${key}" => merge(val, { exec = "agent" }) }
-  )
-
-  name               = "${var.name}-${each.value.exec}-${each.key}"
-  server_type        = each.value.type
-  location           = each.value.location
-  image              = data.hcloud_image.this.id
-  ssh_keys           = [hcloud_ssh_key.this.id]
-  placement_group_id = hcloud_placement_group.this.id
-  labels             = { "rke/${each.value.exec}" = var.name }
-
-  connection {
-    type        = "ssh"
-    host        = self.ipv4_address
-    user        = "root"
-    private_key = tls_private_key.this.private_key_openssh
-    timeout     = "4m"
-  }
-
-  provisioner "remote-exec" {
-    inline = ["cloud-init status --wait"]
-  }
-}
-
-resource "hcloud_server_network" "this" {
-  for_each = hcloud_server.this
-
-  server_id  = each.value.id
-  network_id = var.hcloud_network
-}
-
 module "cluster" {
   source  = "cktf/rke/module"
-  version = "1.20.0"
+  version = "1.20.1"
 
   type       = var.type
   channel    = var.channel
---
+++ servers.tf
@@ -0,0 +1,47 @@
+data "hcloud_image" "this" {
+  name        = "ubuntu-22.04"
+  most_recent = true
+}
+
+resource "hcloud_placement_group" "this" {
+  name = var.name
+  type = "spread"
+}
+
+resource "hcloud_server" "this" {
+  lifecycle {
+    ignore_changes = [image]
+  }
+
+  for_each = merge(
+    { for key, val in var.servers : "server_${key}" => merge(val, { exec = "server", key = key }) },
+    { for key, val in var.agents : "agent_${key}" => merge(val, { exec = "agent", key = key }) }
+  )
+
+  name               = "${var.name}-${each.value.exec}-${each.value.key}"
+  server_type        = each.value.type
+  location           = each.value.location
+  image              = data.hcloud_image.this.id
+  ssh_keys           = [hcloud_ssh_key.this.id]
+  placement_group_id = hcloud_placement_group.this.id
+  labels             = { "rke/${each.value.exec}" = var.name }
+
+  connection {
+    type        = "ssh"
+    host        = self.ipv4_address
+    user        = "root"
+    private_key = tls_private_key.this.private_key_openssh
+    timeout     = "4m"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["cloud-init status --wait"]
+  }
+}
+
+resource "hcloud_server_network" "this" {
+  for_each = hcloud_server.this
+
+  server_id  = each.value.id
+  network_id = var.hcloud_network
+}
